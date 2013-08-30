f1ce7d818974e521868c005022ad7ebfed98f843
kpt 財家孝佑 <zaike@key-p.co.jp>
2013-08-30 10:14:35 +0900

    画面追加

--- GitInfoAppTest/src/com/example/gitinfoapp/test/ListActivityTest.java
+++ GitInfoAppTest/src/com/example/gitinfoapp/test/ListActivityTest.java
@@ -1,8 +1,13 @@
 package com.example.gitinfoapp.test;
 
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Intent;
 import android.os.Handler;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.widget.Button;
 
+import com.example.gitinfoapp.HogeActivity;
 import com.example.gitinfoapp.ListActivity;
 import com.example.gitinfoapp.common.Const;
 import com.example.gitinfoapp.task.ListTask;
@@ -14,7 +19,6 @@ import com.example.gitinfoapp.task.ListTask;
  *
  */
 public class ListActivityTest extends ActivityInstrumentationTestCase2<ListActivity> {
-
 	private ListActivity listActivity;
 
 	private Handler handler;
@@ -54,6 +58,29 @@ public class ListActivityTest extends ActivityInstrumentationTestCase2<ListActiv
 			Thread.sleep(1000);
 		} while (!listTask.isComp());
 
+		// 一覧取得成レスポンスが取得できたかどうか？？
 		assertEquals(Const.LIST_GET_LIST_TASK_SUCCEED, listTask.getMessage().what);
 	}
+	
+	public void testActivity() throws Exception {
+		ActivityMonitor monitorMain = new ActivityMonitor(ListActivity.class.getCanonicalName(), null, false);
+		ActivityMonitor monitorHoge = new ActivityMonitor(HogeActivity.class.getCanonicalName(), null, false);
+		getInstrumentation().addMonitor(monitorMain);
+		getInstrumentation().addMonitor(monitorHoge);
+		
+		final Button button = (Button)this.listActivity.findViewById(com.example.gitinfoapp.R.id.button1);
+		TouchUtils.tapView(this, button);
+		getInstrumentation().waitForMonitorWithTimeout(monitorHoge, 2000);
+		
+		// 指定のActivityに遷移した事を確認する。
+		assertEquals(1, monitorHoge.getHits());
+/*		
+		button.performClick();
+		
+		getInstrumentation().waitForIdleSync();
+		
+		Intent target = getStartedActivityIntent();
+*/
+	}
+
 }
